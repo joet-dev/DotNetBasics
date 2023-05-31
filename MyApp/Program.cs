e5ef37e75795d6a5037175e127c5259d65f8ea24
joet-dev <joseph.thurlow7d0@gmail.com>
2023-05-31 19:31:14 +1000

    first commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random dice = new Random(); 
+int roll = dice.Next(1,7); 
+Console.WriteLine($"TEST: {roll}"); 
---
+++ MyWebApi/README.txt
@@ -0,0 +1,2 @@
+To run the webapi: dotnet run (in the program root folder)
+To see the generated weather forecast (JSON) add /weatherforecast to the end of the URL. 
